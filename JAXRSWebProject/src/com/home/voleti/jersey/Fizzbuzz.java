4b072c5af49a09f5eee41984c0145dc07f9102f8
voletism <smsvoleti@gmail.com>
2016-08-06 20:04:51 +0300

    Update Fizzbuzz.java

--- JAXRSWebProject/src/com/home/voleti/jersey/Fizzbuzz.java
+++ JAXRSWebProject/src/com/home/voleti/jersey/Fizzbuzz.java
@@ -29,11 +29,7 @@ package com.home.voleti.jersey;
 		  public String getFizzbuzzNumber(@PathParam("number1") String number1,
 				  @PathParam("number2") String number2) {
 			  
-			  System.out.println("number1=" + number1 + "number2=" + number2);
-			  
 			String printString = getStringFromInputNumber(number1, number2);
-			
-			System.out.println("printString=" + printString);
 			
 		    return "<html> " + "<title>" + "Hello Jersey" + "</title>"
 		        + "<body><h1>" + printString + "</body></h1>" + "</html> ";
@@ -41,42 +37,39 @@ package com.home.voleti.jersey;
 		  
 		  public static String getStringFromInputNumber (String from, String to) {
 		  
-		  StringBuffer myBuffer = new StringBuffer();
-	      int fromValue = 1;
-	      int toValue = 1;
+		  	StringBuffer myBuffer = new StringBuffer();
+	      		int fromValue = 1;
+	      		int toValue = 1;
 	      
-	      if  (from != null && to != null) {		    	      
-	    	  fromValue = Integer.parseInt(from);
-	    	  toValue = Integer.parseInt(to);
-	      } 
-	      else if (from != null && to == null) {
-	    	   fromValue = Integer.parseInt(from);
-	    	   toValue = fromValue;
-	       }		       
-	      else if (from == null ) {
-	    	   fromValue  = 1;
-	    	   toValue = 1;
-	      }  
+	      		if  (from != null && to != null) {		    	      
+	    	  		fromValue = Integer.parseInt(from);
+	    	  		toValue = Integer.parseInt(to);
+	      		} 
+	      		else if (from != null && to == null) {
+	    	   		fromValue = Integer.parseInt(from);
+	    	   		toValue = fromValue;
+	       		}		       
+	      		else if (from == null ) {
+	    	   		fromValue  = 1;
+	    		 	toValue = 1;
+	      		}  
 	      
-	      for (int i = fromValue;  i <= toValue; i++) {
-	    	  if (i % 3 == 0 && i % 5 == 0) {
-	            // When i is divisible by 3 and divisible by 5, then print "FizzBuzz"
-	            myBuffer.append(" FizzBuzz ");
-	    	  } else if (i % 5 == 0) {
-	            // When i is divisible by 5, then print "Buzz"
-	            
-	            myBuffer.append(" Buzz ");
-	    	  } else if ( i % 3 == 0 ) {
-	    		  // when i is divisible by 3, then print "Fizz"
-	    		  
-	    		  myBuffer.append(" Fizz ");
-	    	  } else {
-	    		   // myBuffer.append(new String (Integer.toString(i)));
-	    		   myBuffer.append(i);
-	    		   myBuffer.append(" ");
-	    	  }
-	      }
-	      return myBuffer.toString();
-	   }
+	      		for (int i = fromValue;  i <= toValue; i++) {
+	    	  		if (i % 3 == 0 && i % 5 == 0) {
+	            			// When i is divisible by 3 and divisible by 5, then print "FizzBuzz"
+	            			myBuffer.append(" FizzBuzz ");
+	    	  		} else if (i % 5 == 0) {
+	            		// When i is divisible by 5, then print "Buzz"
+	            		myBuffer.append(" Buzz ");
+	    	  		} else if ( i % 3 == 0 ) {
+	    		  	// when i is divisible by 3, then print "Fizz"
+	    		  	myBuffer.append(" Fizz ");
+	    	  		} else {
+	    		   	// myBuffer.append(new String (Integer.toString(i)));
+	    		   	myBuffer.append(i);
+	    		   	myBuffer.append(" ");
+	    	  		}
+	      		}
+	      		return myBuffer.toString();
+	   	}
 	}
-
