bbb6fb928f26f1bef9d569c78ffb07e9a4e1f1b8
voletism <smsvoleti@gmail.com>
2016-08-06 18:17:41 +0300

    fizzbuzz heroku application deployed source code

---
+++ Heroku-fizzbuzz-app/src/main/java/com/voleti/FizzBuzzSingle.java
@@ -0,0 +1,38 @@
+package com.voleti;
+
+import java.lang.annotation.Repeatable;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+//Plain old Java Object it does not extend as class or implements 
+//an interface
+
+//The class registers its methods for the HTTP GET request using the @GET annotation. 
+//Using the @Produces annotation, it defines that it can deliver several MIME types,
+//text, XML and HTML. 
+
+//The browser requests per default the HTML MIME type.
+
+
+//Sets the path to base URL + /fizzbuzz
+@Path("/fizzbuzz/{number1}")
+public class FizzBuzzSingle {
+
+	  // This method is called if HTML is request
+	  @GET
+	  @Produces(MediaType.TEXT_HTML)
+	  public String getFizzbuzzNumber(@PathParam("number1") String number1 ) {
+		  
+		String printString = FizzBuzzRange.getStringFromInputNumber(number1,number1);
+		
+		System.out.println("printString=" + printString);
+		
+	    return "<html> " + "<title>" + "Hello Jersey" + "</title>"
+	        + "<body><h1>" + printString + "</body></h1>" + "</html> ";
+	  }
+}
+
